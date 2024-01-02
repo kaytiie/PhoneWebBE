93b6474e0e20b19d7bb1f5d4e2da409e52c545bd
Tran Dinh Thi <gigidream9pro@gmail.com>
2024-01-02 17:04:24 +0700

    thay doi cross

--- src/index.js
+++ src/index.js
@@ -28,13 +28,13 @@ let connectedUsers = 0; // Biến đếm số người đang kết nối
 
 
 const port = process.env.PORT || 3001
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: 'https://shopdientu.vercel.app' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.json())
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cookieParser())
 routes(app);
 
